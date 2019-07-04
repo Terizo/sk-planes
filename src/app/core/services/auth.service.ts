201910d1915d38d5a2f1188f5690d10784039cbb
adpawlik <adpawlik@gmail.com>
2019-07-04 14:29:52 +0200

    Finally app with some error

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { UserInfo } from 'firebase';
 
+export interface Credentials {
+    email: string;
+    password: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  readonly authState$ = this.fireAuth.authState;
+
   private userData: UserInfo;
   constructor(
     private fireAuth: AngularFireAuth
   ) { }
 
-  login(credentials: {email: string, password: string}) {
+  login(credentials: Credentials) {
     return this.fireAuth.auth.signInWithEmailAndPassword(credentials.email, credentials.password)
       .then(userCrediendial => this.userData = userCrediendial.user)
   }
--- src/app/flights/flight-card/flight-card.component.ts
+++ src/app/flights/flight-card/flight-card.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy  } from '@angular/core';
 import { Flight } from './../../model/flight.model';
 
 @Component({
   selector: 'app-flight-card',
   templateUrl: './flight-card.component.html',
-  styleUrls: ['./flight-card.component.scss']
+  styleUrls: ['./flight-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightCardComponent {
   @Input () flight: Flight;
-
+  math = Math;
 }
